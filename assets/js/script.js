ff3ace5d294b71dd6697f655c2ef7cad3fe77f2b
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2023-12-04 13:40:43 -0800

    toggle fav btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,13 @@ const /** {NodeElement} */ $header = document.querySelector("[data-header]");
 window.addEventListener("scroll", e => {
     $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 })
+
+// add to favorite button toggle
+
+const $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active");
+    });
+})
